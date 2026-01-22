e00d91315d668597bdeaafa0fdde2fde951479d1
123llk123 <1091874054@qq.com>
2026-01-22 14:30:02 +0800

    Add files via upload

---
+++ UrlSettings.js
@@ -0,0 +1,10 @@
+class UrlSettings{
+    static localURL = '服务器端地址'
+    static fileUrl = '服务器端地址'
+    // static localURL = 'http://172.26.168.61:9408'
+    // static fileUrl = 'http://172.26.168.61:9408'
+    // static localURL = 'http://39.101.137.10:9408'
+    // static fileUrl =  'http://39.101.137.10:9408'
+}
+
+export default UrlSettings
